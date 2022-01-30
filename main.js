f1ab45eddae66a124376629aa2cf1e74657d68b0
AtharvSaraswat123 <87174532+AtharvSaraswat123@users.noreply.github.com>
2022-01-30 21:43:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+quick_draw_data_set = ['book', 'pencil', 'paper', 'bottle', 'alarm', 'apple', 'banana', 'alarm clock'];
+random_no = Math.floor((Math.random() * quick_draw_data_set.length) + 1)
+console.log(quick_draw_data_set[random_no]);
